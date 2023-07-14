3721c1ef48b40ce2be03b477b552a7743079e8af
Clyde <47860813+Clydeston@users.noreply.github.com>
2023-07-14 18:29:41 +0100

    first commit

---
+++ parse.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <Windows.h>
+#include <fstream>
+#include <winnt.h>
+
+class Parse
+{
+public:
+
+	IMAGE_DOS_HEADER* pDosHeader;
+	IMAGE_NT_HEADERS* pNtHeader;
+	IMAGE_FILE_HEADER* pFileHeader;
+	IMAGE_OPTIONAL_HEADER* pOptionalHeader;
+
+	const char* filePath;	
+	PBYTE pFile;
+
+	bool IsFileValid();
+	void GetFileInfo();
+private:
+	void GetFileHeaderInfo();
+};
+
